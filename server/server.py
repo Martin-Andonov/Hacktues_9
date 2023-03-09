344e75596bcb0086dcdaf997ade0969995a106b3
Martin-Andonov <85078216+Martin-Andonov@users.noreply.github.com>
2023-03-09 13:32:04 +0200

    changed server to handle multiple clients + added a test code

--- server/server.py
+++ server/server.py
@@ -1,20 +1,39 @@
 import socket
 import threading
 
+def multi_threaded_client(connection):
+    connection.send(str.encode('Server is working:'))
+    while True:
+        data = connection.recv(2048)
+        response = 'Server message: ' + data.decode('utf-8')
+        if not data:
+            break
+        connection.sendall(str.encode(response))
+    connection.close()
+
 def launchServer():
 
     TCP_IP = '127.0.0.1'
     TCP_PORT = 7005
+    ThreadCount = 0
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((TCP_IP, TCP_PORT))
+
+    try:
+        s.bind((TCP_IP, TCP_PORT))
+    except socket.error as e:
+        print(str(e))
+
     s.listen(1)
 
-    print('waiting for connection')
-    conn, addr = s.accept()
-
-    print('Connection address:', addr)
+    while True:
+        Client, address = s.accept()
+        print('Connected to: ' + address[0] + ':' + str(address[1]))
+        #start_new_thread(multi_threaded_client, (Client,))
+        ThreadCount += 1
+        print('Thread Number: ' + str(ThreadCount))
+    ServerSideSocket.close()
 
 
 if __name__ == "__main__":
