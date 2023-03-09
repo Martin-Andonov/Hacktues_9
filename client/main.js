43dbc1e78a221353ed9d12b6859566a4399f8299
MrGoblings <emomomchev@gmail.com>
2023-03-09 19:07:11 +0200

    some work on the client

--- client/main.js
+++ client/main.js
@@ -5,20 +5,24 @@ document.getElementById("button").addEventListener("click", function() {
   console.log("Button clicked, isActive = " + isActive);
 });
 
+const LOCATION = "10.1.79.139:9999";
+const ws = new WebSocket(`ws://${LOCATION}`);
 
-const LOCATION = "10.1.79.139:9999"
+ws.addEventListener('open', (event) => {
+  console.log('Connected to server');
+});
 
-
+ws.addEventListener('message', (event) => {
+  console.log('Message from server: ', event.data);
+}); 
 
 function sendLocation() {
-    if(isActive) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            console.log(position.coords.latitude, position.coords.longitude);
-            // serverSocket.send(`${position.coords.latitude} ${position.coords.longitude}`)
-        });
-    }
+  if(isActive) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position.coords.latitude, position.coords.longitude);
+      ws.send(`${position.coords.latitude}|${position.coords.longitude}`);
+    });
+  }
 }
 
-
 setInterval(sendLocation, 5000);
-
