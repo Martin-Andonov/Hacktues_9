ed72f2e948d5683ed470af54413b0045d7049bd8
MrGoblings <emomomchev@gmail.com>
2023-04-23 08:34:27 +0300

    adding location

--- resqme_app/lib/utils.dart
+++ resqme_app/lib/utils.dart
@@ -7,6 +7,7 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:web_socket_channel/io.dart';
 
 const String serverLocation = 'wss://echo.websocket.events';
+const int secondsToReconnect = 1;
 
 class LocationWebSocket {
   WebSocketChannel _channel;
@@ -24,18 +25,45 @@ class LocationWebSocket {
     });
   }
 
-  Future<String> sendLocation(LocationData location) async {
-    while (_channel.closeCode == null) {
-      try {
-        _channel.sink.add(location.toString());
-        final message = await _messageStreamController.stream.first;
-        return message;
-      } catch (e) {
-        debugPrint('Error sending location to server: $e');
-        await Future.delayed(const Duration(seconds: 1));
+  Future<void> _checkLocationPermissions() async {
+    final location = Location();
+
+    // Check if location services are enabled
+    bool serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        throw Exception('Location services are not enabled');
       }
     }
-    throw Exception('WebSocket closed');
+
+    // Check if location permissions are granted
+    PermissionStatus permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        throw Exception('Location permissions are not granted');
+      }
+    }
+  }
+
+  Future<String> sendLocationToServer(LocationData location) async {
+    // Check location permissions before sending data
+    await _checkLocationPermissions();
+
+    while (true) {
+      try {
+        final completer = Completer<String>();
+        _channel.sink.add(location.toString());
+        _channel.stream.listen((event) {
+          completer.complete(event);
+        });
+        return completer.future;
+      } catch (e) {
+        debugPrint('Error sending location to server: $e');
+        await _reconnect();
+      }
+    }
   }
 
   Future<LocationData> getCurrentLocation() async {
@@ -48,16 +76,16 @@ class LocationWebSocket {
     }
   }
 
-  void _reconnect() async {
+  Future<void> _reconnect() async {
     while (_channel.closeCode != null) {
       try {
         debugPrint('WebSocket reconnecting...');
         await _channel.sink.close(WebSocketStatus.goingAway);
-        await Future.delayed(const Duration(seconds: 1));
+        await Future.delayed(const Duration(seconds: secondsToReconnect));
         _channel = IOWebSocketChannel.connect(serverLocation);
       } catch (e) {
         debugPrint('WebSocket reconnection error: $e');
-        await Future.delayed(const Duration(seconds: 1));
+        await Future.delayed(const Duration(seconds: secondsToReconnect));
       }
     }
   }
